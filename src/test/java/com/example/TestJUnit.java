13be7021af8d2a1981ffa3e9daa75d41f838c647
l.abburi <l.abburi@BDC3-D-519QSWY.dir.svc.accenture.com>
2016-12-27 11:59:35 +0530

    adding testcase

---
+++ src/test/java/com/example/TestJUnit.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestJUnit {
+
+	@Test
+	public void test() {
+		String str = "Junit is working fine";
+		assertEquals("Junit is working finetttt",str);
+	}
+
+}
